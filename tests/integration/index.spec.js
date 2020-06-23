aba7b02da3a8fbd197d999b15da137696cd5bf31
Guilherme Victal <guilhermevictal@gmail.com>
2020-06-22 22:13:05 -0300

    Add cypress eslint plugin

--- tests/integration/index.spec.js
+++ tests/integration/index.spec.js
@@ -7,25 +7,27 @@ describe('Cypress tests for extension UI changes', () => {
 
       // TODO: Get adjacent subscribe button and validate same css except for a ignoreList
       // Or add visual validation
-      const rssButtons = cy.get('.button-rss')
-      rssButtons.should('contain', 'RSS')
-      rssButtons.should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
+      cy.get('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
     })
 
     it(`Should find an RSS button on the last episode page for ${info.name}'s sidebar`, () => {
       cy.visit(`https://tapas.io/series/${info.name}`)
-      const subscribeButtons = cy.get('.side-section').get('.subscribe-btn')
-      const rssButtons = subscribeButtons.parent().children('.button-rss')
-      rssButtons.should('contain', 'RSS')
-      rssButtons.should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
+      cy.get('.side-section')
+        .get('.subscribe-btn').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
     })
 
     it(`Should find an RSS button on a ${info.name} episode page sidebar`, () => {
       cy.visit(`https://tapas.io/episode/${info.episode}`)
-      const subscribeButtons = cy.get('.side-section').get('.subscribe-btn')
-      const rssButtons = subscribeButtons.parent().children('.button-rss')
-      rssButtons.should('contain', 'RSS')
-      rssButtons.should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
+      cy.get('.side-section')
+        .get('.subscribe-btn').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
     })
 
     it(`Should find an RSS button in the /info popup from the ${info.name} episode page`, () => {
@@ -34,10 +36,11 @@ describe('Cypress tests for extension UI changes', () => {
       cy.location().should(loc => {
         expect(loc.pathname).to.be.equal(`/series/${info.name}/info`)
       })
-      const subscribeButtons = cy.get('.modal-backdrop').get('.button--subscribe')
-      const rssButtons = subscribeButtons.parent().children('.button-rss')
-      rssButtons.should('contain', 'RSS')
-      rssButtons.should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
+      cy.get('.modal-backdrop')
+        .get('.button--subscribe').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}`)
     })
   }
 })
