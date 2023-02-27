cf935f8dcce20e8f2812964ffe39ab77d33bd429
Guilherme Victal <guilhermevictal@gmail.com>
2023-02-26 21:31:34 -0300

    feat: add option to copy link to clipboard

---
+++ tests/integration/copy.spec.js
@@ -0,0 +1,58 @@
+const { testPages } = require('../utils/series')
+
+describe('Cypress tests for extension UI changes', () => {
+  beforeEach(() => cy.setExtensionStorage('sync', { rssAction: 'copy' })
+  )
+  afterEach(() => cy.clearExtensionStorage('sync'))
+
+  for (const info of testPages) {
+    it.only(`Should find an RSS button accessing the info page directly for ${info.name}`, () => {
+      cy.visit(`https://tapas.io/series/${info.name}/info`)
+
+      // TODO: Get adjacent subscribe button and validate same css except for a ignoreList
+      // Or add visual validation
+      cy.get('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}/`).each(button => {
+        cy.get(button).click()
+        .and('contain', 'Copied!')
+          cy.window().its('navigator.clipboard')
+          .then((clip) => clip.readText())
+          .should('equal', 'npm install -D cypress')
+        })
+
+    })
+
+    it(`Should find an RSS button on the last episode page for ${info.name}'s sidebar`, () => {
+      cy.visit(`https://tapas.io/series/${info.name}`)
+      cy.get('.side-section')
+        .get('.subscribe-btn').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}/`)
+    })
+
+    it(`Should find an RSS button on a ${info.name} episode page sidebar`, () => {
+      cy.visit(`https://tapas.io/episode/${info.episode}`)
+      cy.get('.side-section')
+        .get('.subscribe-btn').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}/`)
+    })
+
+    it(`Should find an RSS button in the /info popup from the ${info.name} episode page`, () => {
+      cy.visit(`https://tapas.io/episode/${info.episode}`)
+      cy.get('.side-section').get('.js-series-btn').first().click()
+      cy.location().should(loc => {
+        expect(loc.pathname).to.be.equal(`/series/${info.name}/info`)
+      })
+      cy.get('.modal-backdrop')
+        .get('.button--subscribe').parent()
+        .children('.button-rss')
+        .should('contain', 'RSS')
+        .should('have.attr', 'href', `https://tapas.io/rss/series/${info.id}/`)
+    })
+  }
+})
+
--- tests/integration/support/commands.js
+++ tests/integration/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const addExtensionCommands = require('@victal/cypress-extensions-plugin/commands');
+addExtensionCommands(Cypress);
